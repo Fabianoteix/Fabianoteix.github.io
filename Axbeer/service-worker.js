9a6a7c6e51534c4de8b4bcce9a27aeafb619ea4a
Fabteix <fabianotcarvalho@gmail.com>
2017-09-27 14:59:27 -0300

    a

--- Axbeer/service-worker.js
+++ Axbeer/service-worker.js
@@ -26,7 +26,7 @@ if (typeof files == 'undefined') {
   files.push('./');
 }
 
-var CACHE_NAME = 'axbeer-3.2';
+var CACHE_NAME = 'axbeer-3.3';
 
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activate');
